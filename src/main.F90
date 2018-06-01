4fe49e445e37bd6dad5ba9d9bc8bd41f903df59a
Tomasz Kasprzycki <kasprzyckit@gmail.com>
2018-06-01 15:31:59 +0200

    parts 1&2 done

--- src/main.F90
+++ src/main.F90
@@ -1,14 +1,22 @@
 program main
 
-    use matrix_multiplcation, only : mm
+    use matrix_multiplication, only : mm0, mm1, mm2, mm3
 
 
     real(kind= 8), allocatable :: first(:, :), second(:, :), result(:, :)
-    integer(kind=4) :: i, j, status, n, m, k
+    real :: start, finish
+    integer(kind=4) :: i, j, status, n, m, k, ichunk
+    integer(kind=4) :: l, s
+    character(len=10) :: buffer
 
-    n = 2
-    m = 4
-    k = 3
+    call get_command_argument(1, buffer, l, s)
+    read(buffer,*) N
+
+    call get_command_argument(1, buffer, l, s)
+    read(buffer,*) ichunk
+
+    m = max(n - 10, 1)
+    k = max(n*2/3, 1)
 
     allocate(first(n, k))
     allocate(second(k, m))
@@ -16,7 +24,7 @@ program main
 
     do i=1,n
         do j=1,k
-            first(i, j) = i*j + 1
+            first(i, j) = i*j + 10
         enddo
     enddo
 
@@ -26,12 +34,41 @@ program main
         enddo
     enddo
 
-    call mm(first, second, result, status)
+    do i=20, 300, 40
 
-    write (*, *) first(2, :)
-    write (*, *) ""
-    write (*, *) second(:, 4)
-    write (*, *) ""
-    write (*, *) result(2,4)
+        call cpu_time(start)
+        call mm3(first, second, result, status)
+        call cpu_time(finish)
+        write (*, *) i
+        print '("Time mm2 = ",f6.3," seconds.")',finish-start
+
+        ! call cpu_time(start)
+        ! call mm3(first, second, result, status, i)
+        ! call cpu_time(finish)
+        ! write (*, *) i
+        ! print '("Time mm3 = ",f6.3," seconds.")',finish-start
+
+    enddo
+
+    ! write (*, *) 
+    ! first(2, :)
+    ! write (*, *) ""
+    ! write (*, *) second(:, 4)
+    ! write (*, *) ""
+    ! write (*, *) result(2,4)
+    ! write (*, *) ""
+
+    ! call cpu_time(start)
+    ! call mm0(first, second, result, status)
+    ! call cpu_time(finish)
+
+    ! print '("Time mm0 = ",f6.3," seconds.")',finish-start
+
+    ! call cpu_time(start)
+    ! call mm1(first, second, result, status)
+    ! call cpu_time(finish)
+
+    ! print '("Time mm1 = ",f6.3," seconds.")',finish-start
+
 
 end program main
--- src/matrix_multiplication.F90
+++ src/matrix_multiplication.F90
@@ -1,47 +1,145 @@
-module matrix_multiplcation        
+module matrix_multiplication        
 contains
 
-function dotp(first, second, N) result(product)
-
+subroutine mm0(first, second, multiply, status)
     implicit none
-    real(kind= 8) :: first(:)
-    real(kind= 8) :: second(:)
-    real(kind= 8) :: product
-    integer(kind= 4) :: N, I
-
-    product = 0
-
-    do I=1, N
-        product = product + first(I) * second(I)
-    enddo
-
-end function
-
-subroutine mm(first, second, multiply, status)
-    implicit none
-    real(kind= 8),intent(in):: first(:,:) ! pierwsza macierz
-    real(kind= 8),intent(in):: second(: ,:) ! druga macierz
-    real(kind= 8),intent(out):: multiply(:,:) ! macierz wynikowa
-    integer(kind= 4),intent(out):: status ! kod błędu, 0 gdy OK
-    integer(kind=4) :: I, J, N, M, shape1(2), shape2(2), shape3(2)
+    real(kind= 8),intent(in):: first(:,:)
+    real(kind= 8),intent(in):: second(: ,:)
+    real(kind= 8),intent(out):: multiply(:,:)
+    integer(kind= 4),intent(out):: status
+    integer(kind=4) :: i, j, k, shape1(2), shape2(2), shape3(2)
 
     shape1 = shape(first)
     shape2 = shape(second)
     shape3 = shape(multiply)
 
-    ! if (shape1(2) .NE. shape2(1)) 
-    ! if (shape3(1) .NE. shape1(1) .OR. shape3(2) .NE. shape2(2))
+    if (shape1(2) .NE. shape2(1)) then
+        status = 1
+        return
+    endif
+    if (shape3(1) .NE. shape1(1) .OR. shape3(2) .NE. shape2(2)) then
+        status = 2
+        return
+    endif
 
-    N = shape1(1)
-    M = shape2(2)
+    multiply = 0
 
-    do I=1,N
-        do J=1,M
-            multiply(I, J) = dotp(first(I, :), second(:, J), shape1(2))
+    do i=1,shape1(1)
+        do j=1,shape2(2)
+            do k=1,shape1(2)
+                multiply(i, j) = multiply(i, j) + first(i, k) * second(k, j)
+            enddo
         enddo
     enddo
     status = 0
 
 end subroutine
 
-end module matrix_multiplcation
+subroutine mm1(first, second, multiply, status)
+    implicit none
+    real(kind= 8),intent(in):: first(:,:)
+    real(kind= 8),intent(in):: second(: ,:)
+    real(kind= 8),intent(out):: multiply(:,:)
+    integer(kind= 4),intent(out):: status
+    integer(kind=4) :: i, j, shape1(2), shape2(2), shape3(2)
+
+    shape1 = shape(first)
+    shape2 = shape(second)
+    shape3 = shape(multiply)
+
+    if (shape1(2) .NE. shape2(1)) then
+        status = 1
+        return
+    endif
+    if (shape3(1) .NE. shape1(1) .OR. shape3(2) .NE. shape2(2)) then
+        status = 2
+        return
+    endif
+
+
+    do i=1,shape1(1)
+        do j=1,shape2(2)
+            multiply(i, j) = dot_product(first(i,:), second(:,j))
+        enddo
+    enddo
+    status = 0
+
+end subroutine
+
+subroutine mm2(first, second, multiply, status)
+    implicit none
+    real(kind= 8),intent(in):: first(:,:)
+    real(kind= 8),intent(in):: second(: ,:)
+    real(kind= 8),intent(out):: multiply(:,:)
+    integer(kind= 4),intent(out):: status
+    integer(kind=4) :: i, j, k, jj, kk, shape1(2), shape2(2), shape3(2), ichunk
+
+    shape1 = shape(first)
+    shape2 = shape(second)
+    shape3 = shape(multiply)
+
+    if (shape1(2) .NE. shape2(1)) then
+        status = 1
+        return
+    endif
+    if (shape3(1) .NE. shape1(1) .OR. shape3(2) .NE. shape2(2)) then
+        status = 2
+        return
+    endif
+
+    ichunk = 180
+
+    multiply = 0
+
+    do jj = 1, shape2(2), ichunk
+        do kk = 1, shape1(2), ichunk
+            do j = jj, min(jj + ichunk - 1, shape2(2))
+                do k = kk, min(kk + ichunk - 1, shape1(2))
+                    do i = 1,shape1(1)
+                        multiply(i, j) = multiply(i, j) + first(i, k) * second(k, j)
+                    end do
+                end do
+            end do
+        end do
+    end do
+    status = 0
+
+end subroutine
+
+subroutine mm3(first, second, multiply, status)
+    implicit none
+    real(kind= 8),intent(in):: first(:,:)
+    real(kind= 8),intent(in):: second(: ,:)
+    real(kind= 8),intent(out):: multiply(:,:)
+    integer(kind= 4),intent(out):: status
+    integer(kind=4) :: jj, ii, i, j, shape1(2), shape2(2), shape3(2), ichunk
+
+    shape1 = shape(first)
+    shape2 = shape(second)
+    shape3 = shape(multiply)
+
+    if (shape1(2) .NE. shape2(1)) then
+        status = 1
+        return
+    endif
+    if (shape3(1) .NE. shape1(1) .OR. shape3(2) .NE. shape2(2)) then
+        status = 2
+        return
+    endif
+
+    ichunk = 180
+
+    do jj = 1, shape2(2), ichunk
+        do ii = 1, shape1(1), ichunk
+            do j = jj, min(jj + ichunk - 1, shape2(2))
+                do i = ii, min(ii + ichunk - 1, shape1(1))
+                    multiply(i, j) =  dot_product(first(i,:), second(:,j))
+                end do
+            end do
+        enddo
+    end do
+    status = 0
+
+end subroutine
+
+end module matrix_multiplication
